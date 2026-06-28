f6e54a01d854fc42c82c98ee9547ed490b8dec10
Luc Rurangwa <lucrurangwa@gmail.com>
2026-06-28 11:37:34 +0200

    Fix stock product search icon position

--- apps/web/src/app/(owner)/stock/receive/receive-stock-form.tsx
+++ apps/web/src/app/(owner)/stock/receive/receive-stock-form.tsx
@@ -65,7 +65,7 @@ export function ReceiveStockForm({ products, error }: ReceiveStockFormProps) {
           </label>
 
           <div className="relative">
-            <Search className="pointer-events-none absolute left-3 top-[38px] h-4 w-4 text-slate-400" />
+            <Search className="pointer-events-none absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-slate-400" />
 
             <input
               id="productSearch"
